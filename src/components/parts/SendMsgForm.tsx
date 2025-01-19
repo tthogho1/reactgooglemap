d638f9713438f437562fb1028b6e34c186b1795b
t-tougou <tthogho1@gmail.com>
2025-01-19 18:11:02 +0900

    change ui / add server connet icon

--- src/components/parts/SendMsgForm.tsx
+++ src/components/parts/SendMsgForm.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import ConfirmDialog  from './ConfirmDialog';
-//import { useRTCPeerConnection } from '../RTCPeerConnectionProvider';
 import { useWebSocket } from '../WebSocketProvider';
 import { ChatMessage, Sdp } from '../../types/webrtc';
 import eventBus from '../class/EventBus';
 import { user } from '../../types/map';
 import { updatePositionOnServer } from '../../api/backend';
 import webRtc from '../class/WebRtc';
+import { FaRegFaceSmile, FaFaceSadCry } from "react-icons/fa6";
 
 interface ChildComponentProps {
   // true if called
@@ -21,6 +21,7 @@ const SendMsgForm :React.FC<ChildComponentProps> = ({ openVideoChat,users }) => 
   const { user } = useAuth();
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  const [websocketOpen, setWebsocketOpen] = useState(false);
   const [confirmMessage, setConfirmMessage] = useState('');
   const [sdp, setSdp] = useState<ChatMessage | null>(null);
   const { socket , updateSocket} = useWebSocket();
@@ -79,17 +80,17 @@ const SendMsgForm :React.FC<ChildComponentProps> = ({ openVideoChat,users }) => 
   useEffect(() => {
     if (!socket) {
       alert("WebSocket connection is not established");
-      console.log("WebSocket connection is not established");
+      setWebsocketOpen(false);
+      //console.log("WebSocket connection is not established");
       return;
     }
     if (socket && socket.readyState === WebSocket.OPEN) {
-      // alert("WebSocket connection is established");
-      appendMessage('System: Established WebSocket connection');
+      setWebsocketOpen(true);
     }
 
     socket.onopen = function(event) {
         console.log("WebSocket connection is opened");
-        appendMessage('System: Establish WebSocket connection on open');
+        setWebsocketOpen(true);
     };
   
     socket.onmessage = function(event) {
@@ -103,12 +104,10 @@ const SendMsgForm :React.FC<ChildComponentProps> = ({ openVideoChat,users }) => 
             break;
           case 'answer':
             console.log(`receive answer from ${data.user_id}`);
-            // eventBus.emit('setAnswer', data);
             webRtc.setAnswer(data);
             break;
           case 'ice':
             console.log(`receive ice from ${data.user_id} at ` + new Date());
-            //eventBus.emit('setCandidate', data);
             webRtc.setCandidate(data);
             break;
           case 'close':
@@ -131,11 +130,13 @@ const SendMsgForm :React.FC<ChildComponentProps> = ({ openVideoChat,users }) => 
           }
         }, 2000);
       }    
+      setWebsocketOpen(false);
       appendMessage('System: WebSocket connection is closed');
     };
 
     socket.onerror = function(error) {
         console.error(`WebSocket connection error: ${error}`);
+        setWebsocketOpen(false);
     }
 
     return () => {  
@@ -144,25 +145,32 @@ const SendMsgForm :React.FC<ChildComponentProps> = ({ openVideoChat,users }) => 
 
   return (
     <div className="flex flex-col items-center space-y-2 p-4 bg-gray-100 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-2">
-      <div className="flex-grow h-64 overflow-y-auto bg-white p-4 rounded-md border border-gray-300 md:w-full">
-        {messages.map((msg, index) => (
-          <div key={index} className="mb-2">{msg}</div>
-        ))}
-        <div ref={messagesEndRef} />
+      <div className="flex-grow md:w-full w-full">
+        <div className="w-full md:flex md:justify-start md:items-start mb-2">
+          {websocketOpen ?  <FaRegFaceSmile size={18} className="md:w-1/6" /> : <FaFaceSadCry className="md:w-1/6" />}
+        </div> 
+        <div className="flex-grow h-48 overflow-y-auto bg-white p-4 rounded-md border border-gray-300">
+          <div> 
+            {messages.map((msg, index) => (
+              <div key={index}>{msg}</div>
+            ))}
+            <div ref={messagesEndRef} />
+          </div>
+        </div>
       </div>  
-      <div className="md:flex md:space-x-2 md:w-full"> 
-        <div className="w-full md:flex md:flex-col md:space-y-2">
-          <div className="w-full md:flex md:space-x-2"> 
+      <div className="md:flex md:space-x-2 md:w-full md:h-full">
+        <div className="w-full md:flex md:flex-col  md:justify-start md:space-y-2">
+          <div className="w-full md:flex md:space-x-2">
             <input
               type="text"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               placeholder="Enter Message"
-              className="w-full md:flex-grow px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent md:w-full"
+              className="w-full md:w-3/4 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
             <button
               onClick={handleSetWebsock}
-              className="w-full md:w-1/4 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out md:w-full"
+              className="w-full md:w-1/4 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-outz"
             >
               Send
             </button>
@@ -172,7 +180,7 @@ const SendMsgForm :React.FC<ChildComponentProps> = ({ openVideoChat,users }) => 
             <select
               value={selectedMsgUser}
               onChange={(e) => setSelectedMsgUser(e.target.value)}
-              className="w-full md:w-1/2 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="md:w-1/2 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             >
               <option value="">{BROAD}</option>
               {users.map((user) => (
